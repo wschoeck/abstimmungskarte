2f0ef0371e88d75abae369c1377fcbda032ac2a6
wschoeck <wschoeck@bluewin.ch>
2019-07-09 19:55:30 +0200

    add percent

--- visualization.js
+++ visualization.js
@@ -26,8 +26,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
           const yespercent = container.append("div")
           .style("opacity", 0)
-          .style("position", "fixed")
-          .style("background", "rgba(255,255,255,0.8)")
+          .style("position", "absolute")
+          .style("background", "rgba(37,105,0,0.8)")
+          .style("padding", "0.5rem")
+          .style("pointer-events", "none")
+
+          const percent = container.append("div")
+          .style("opacity", 0)
+          .style("position", "absolute")
+          .style("background", "rgba(208,0,27,0.8)")
           .style("padding", "0.5rem")
           .style("pointer-events", "none")
 
@@ -49,20 +56,31 @@ document.addEventListener("DOMContentLoaded", () => {
             .style("opacity", 1)
             .html(d.name)
           })
-          .on("mousemove", function() {
+          .on("mousemove", function(d) {
             tooltip
             .style("left", d3.event.pageX + 5 + "px")
             .style("top", d3.event.pageY + 5 + "px")
           })
-          .on("mouseleave", function() {
+          .on("mouseleave", function(d) {
             tooltip.style("opacity", 0)
           })
+          .on("mouseleave", function(d) {
+            yespercent.style("opacity", 0)
+          })
           .on("click", function(d) {
+            percent
+            .style("opacity", 1)
+            .html(100 - d.ja_anteil + "%")
+            .style("left", 20 + "px")
+            .style("top", 100 + "px")
             yespercent
             .style("opacity", 1)
-            .html(d.ja_anteil)
+            .html(d.ja_anteil + "%")
+            .style("left", 20 + "px")
+            .style("top", 140 + "px")
           })
-          .on("mouseleave", function() {
+          .on("mouseleave", function(d) {
+            percent.style("opacity", 0)
             yespercent.style("opacity", 0)
           })
           .attr("fill", "#ceccc0")
@@ -89,12 +107,7 @@ document.addEventListener("DOMContentLoaded", () => {
           const results = svg.selectAll("path")
             .data(yesVotes)
             .enter()
-            .attr("d", d => pathGenerator(d))
-            .on("click", function(d) {
-              tooltip
-              .style("opacity", 1)
-              .html(d.ja_anteil)
-            })
+            .attr("d", d => pathGenerator())
 
           /* results */
 
